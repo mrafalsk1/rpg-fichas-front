32582ad2dd0f2cfb9348d50bdc4bef7995c87005
Matheus Rafalski <matheus.rafalski@nuvoni.com.br>
2022-07-20 23:23:46 -0300

    chore: add lint and css normalize

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './styles/global/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      Bora caralho
+    </>
+  )
+}
+
+export default App
